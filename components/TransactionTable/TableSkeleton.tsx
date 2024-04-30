05ba5523ccddc2f390bb5706d41363c1558a7088
Carlos F Arboleda G <arbofercho@gmail.com>
2024-04-30 01:11:26 -0500

    Fixed skeleton theme

--- components/TransactionTable/TableSkeleton.tsx
+++ components/TransactionTable/TableSkeleton.tsx
@@ -2,21 +2,32 @@ import { Skeleton } from "@nextui-org/skeleton";
 
 export const TableSkeleton: React.FC = () => {
   return (
-    <div className="w-full space-y-5 rounded-large p-4 bg-zinc-900">
-      <Skeleton className="rounded-lg">
-        <div className="h-10 rounded-lg bg-default-300"></div>
-      </Skeleton>
+    <div className="w-full space-y-5 rounded-large p-4 bg-slate-100 dark:bg-zinc-900">
+      {renderRow("h-10")}
       <div className="space-y-3">
-        <Skeleton className="rounded-lg">
-          <div className="h-6 rounded-lg bg-default-200"></div>
-        </Skeleton>
-        <Skeleton className=" rounded-lg">
-          <div className="h-6 rounded-lg bg-default-200"></div>
-        </Skeleton>
-        <Skeleton className="rounded-lg">
-          <div className="h-6 rounded-lg bg-default-300"></div>
-        </Skeleton>
+        {renderRow("h-16")}
+        {renderRow("h-16")}
+        {renderRow("h-16")}
       </div>
     </div>
   );
 };
+
+function renderRow(height: string = "h-6") {
+  return (
+    <div className="flex flex-row gap-3">
+      <Skeleton className="rounded-lg w-full">
+        <div className={`${height} rounded-lg bg-default-200`}></div>
+      </Skeleton>
+      {/* <Skeleton className="rounded-lg w-3/4">
+        <div className={`${height} rounded-lg bg-default-200`}></div>
+      </Skeleton>
+      <Skeleton className="rounded-lg w-1/3">
+        <div className={`${height} rounded-lg bg-default-200`}></div>
+      </Skeleton>
+      <Skeleton className="rounded-lg w-1/3">
+        <div className={`${height} rounded-lg bg-default-200`}></div>
+      </Skeleton> */}
+    </div>
+  );
+}
