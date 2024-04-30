5d030ff3033e626beefa8d0ffc2603166a615449
Carlos F Arboleda G <arbofercho@gmail.com>
2024-04-30 02:37:02 -0500

    Finxing Navbar name

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Providers } from "./providers";
-import { Navbar } from "@/components/shared/Navbar";
+import { Navbar } from "@/components/shared/Navbar1";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
