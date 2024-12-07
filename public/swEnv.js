21490d535a5e81acde90a1d2d8e9564830251c95
Carlos F Arboleda G <arbofercho@gmail.com>
2024-12-06 23:25:26 -0500

    Deleted swEnv.js file since it is generated on start up

--- public/swEnv.js
+++
@@ -1,3 +0,0 @@
-const swEnv = {
-    NEXT_PUBLIC_FIREBASE_APP_CONFIG: {"apiKey":"AIzaSyAOzo9VdIsZNpnq7WeXfMNSKZ8pJWopDI0","authDomain":"ai-money-tracker.firebaseapp.com","projectId":"ai-money-tracker","storageBucket":"ai-money-tracker.appspot.com","messagingSenderId":"491863127122","appId":"1:491863127122:web:5da74c423cdc81be90db8b"}
-}
