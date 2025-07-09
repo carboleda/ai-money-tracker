d56410e47f833210b11b73c681134274f153bf54
Carlos F Arboleda G <arbofercho@gmail.com>
2025-07-08 21:24:55 -0500

    Add viewport settings for initial and maximum scale

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  initialScale: 1,
+  maximumScale: 1,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
