00e6d3561b40fd8ef9e5e24c9fb6acaea1e98ae5
Carlos F Arboleda G <arbofercho@gmail.com>
2025-11-05 00:03:36 -0500

    feat: Initialize Firebase app in instrumentation module

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,18 @@
+import { Env } from "./config/env";
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("@/genai/genkitAI");
+
+    // Initialize Firebase App
+    const { initializeApp, cert, getApps } = await import("firebase-admin/app");
+    const alreadyCreatedAps = getApps();
+    if (alreadyCreatedAps.length === 0) {
+      initializeApp({
+        credential: cert(Env.FIREBASE_SERVICE_ACCOUNT),
+      });
+      console.log("Firebase app initialized successfully.");
+    }
 
     // try {
     //   const data = await genkitAI.extractData("Pago de prueba por 2000, AFC");
