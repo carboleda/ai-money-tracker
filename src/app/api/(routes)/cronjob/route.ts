ebac73e1f7c7ff44e3cb09c79f8403f1964640b1
Carlos F Arboleda G <arbofercho@gmail.com>
2025-11-26 18:12:42 -0500

    (refactor): Implement cronjob secret middleware and update route base configurations

--- src/app/api/(routes)/cronjob/route.ts
+++ src/app/api/(routes)/cronjob/route.ts
@@ -1,21 +1,10 @@
 import "reflect-metadata";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { PendingTransactionNotificationService } from "@/app/api/domain/notification/service/pending-transaction-notification.service";
 import { CreateMonthlySummaryService } from "@/app/api/domain/summary/service/create-monthly-summary.service";
 import { api } from "@/app/api";
 
-export async function GET(req: NextRequest) {
-  const authHeader = req.headers.get("authorization");
-  const CRON_SECRET = process.env.CRON_SECRET;
-  const isLocal = process.env.NODE_ENV === "development";
-
-  if (!isLocal && (!CRON_SECRET || authHeader !== `Bearer ${CRON_SECRET}`)) {
-    console.error("Unauthorized request");
-    return new NextResponse(JSON.stringify({ success: false }), {
-      status: 401,
-    });
-  }
-
+export async function GET() {
   const notificationService = api.resolve(
     PendingTransactionNotificationService
   );
--- src/app/api/(routes)/recurring-expenses/schedule/route.ts
+++ src/app/api/(routes)/recurring-expenses/schedule/route.ts
@@ -1,21 +1,9 @@
 import "reflect-metadata";
-import { Env } from "@/config/env";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { ScheduleRecurrentExpenseService } from "@/app/api/domain/recurrent-expense/service/schedule-recurrent-expense.service";
 import { api } from "@/app/api";
 
-export async function GET(req: NextRequest) {
-  const authHeader = req.headers.get("authorization");
-  if (
-    !Env.isLocal &&
-    (!Env.CRON_SECRET || authHeader !== `Bearer ${Env.CRON_SECRET}`)
-  ) {
-    console.error("Unauthorized request");
-    return new NextResponse(JSON.stringify({ success: false }), {
-      status: 401,
-    });
-  }
-
+export async function GET() {
   const service = api.resolve(ScheduleRecurrentExpenseService);
 
   const result = await service.execute();
